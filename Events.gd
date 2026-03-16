588a2cb908a67ed70131d4b1807a2b6d4f0dfabe
Gawor270 <gaworbernard@gmail.com>
2026-03-17 00:27:58 +0100

    rename project, add ignore unused macro

--- Events.gd
+++ Events.gd
@@ -1,5 +1,6 @@
-@warning_ignore("unused_signal")
 extends Node
 
+@warning_ignore("unused_signal")
 signal music_requested(new_stream: AudioStream)
+@warning_ignore("unused_signal")
 signal map_change_requested(new_map_path: String, spawn_point_name: String)
