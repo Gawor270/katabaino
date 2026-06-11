44a83cafd19864a7d854ccfb51071bd0098206b9
Gawor270 <gaworbernard@gmail.com>
2026-06-11 09:57:09 +0200

    show tutorial only once per session

--- maps/starting_room/tutorial.gd
+++ maps/starting_room/tutorial.gd
@@ -4,11 +4,15 @@ var _player: Node
 
 func _ready() -> void:
 	_player = get_parent().find_child("Player", true, false)
+	if PlayerData.tutorial_seen:
+		queue_free()
+		return
 	if _player:
 		_player.process_mode = Node.PROCESS_MODE_DISABLED
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_accept"):
+		PlayerData.tutorial_seen = true
 		if _player:
 			_player.process_mode = Node.PROCESS_MODE_INHERIT
 		queue_free()
--- player/player_data.gd
+++ player/player_data.gd
@@ -3,5 +3,6 @@ extends Node
 # This remembers if we found the item in the world
 var has_bicycle: bool = false
 
-# This remembers if we are currently using it
 var is_riding_bicycle: bool = false
+
+var tutorial_seen: bool = false
